11a71132006aecbd474be32d4b3a2cdb4dca1958
Alexey Likharev <liharev@gmail.com>
2021-09-12 15:17:06 +0300

    HW for lesson7

---
+++ HW7.2.py
@@ -0,0 +1,34 @@
+import random
+a = [random.random()*50 for x in range(10)]
+
+def merge(left, right, compare):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if compare(left[i], right[j]):
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
+
+import operator
+
+def merge_sort(L, compare=operator.lt):
+    if len(L) < 2:
+        return L[:]
+    else:
+        middle = int(len(L) / 2)
+        left = merge_sort(L[:middle], compare)
+        right = merge_sort(L[middle:], compare)
+        return merge(left, right, compare)
+
+print(a)
+print(merge_sort(a))
